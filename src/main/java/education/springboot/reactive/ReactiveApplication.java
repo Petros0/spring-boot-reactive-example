bef1acc67a0cfb7cf139cfb716da633ea25c9d12
Petros Stergioulas <petros.stergioulas94@gmail.com>
2019-01-26 17:33:26 +0100

    init

---
+++ src/main/java/education/springboot/reactive/ReactiveApplication.java
@@ -0,0 +1,14 @@
+package education.springboot.reactive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactiveApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReactiveApplication.class, args);
+    }
+
+}
+
